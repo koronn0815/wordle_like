81ef7788748e7efd26c5b1ceb426777c98c100bb
yoshida kento <kento215@icloud.com>
2022-02-13 17:03:15 +0900

    プロジェクト ファイルを追加します。

---
+++ wordle_like/wordle_like.cpp
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include <iostream>
+#include <string>
+#include "wordle_like.h"
+
+int main()
+{
+    std::string answer;
+
+    generate_string(answer);
+}
---
+++ wordle_like/wordle_like.h
@@ -0,0 +1,3 @@
+#pragma once
+
+std::string generate_string(std::string& answer);
